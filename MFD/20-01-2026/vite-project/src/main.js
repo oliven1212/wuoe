b2365c2d7f26f3c89bff5a2c609c1b8eb7b266aa
Valdemar <pizza27@live.dk>
2026-01-27 11:41:06 +0100

    Lint opsætning 27-01-2026

--- MFD/20-01-2026/vite-project/src/main.js
+++ MFD/20-01-2026/vite-project/src/main.js
@@ -1,1 +1,17 @@
 import './assets/scss/main.scss';
+
+const demo = 'test';
+
+console.log(demo);
+
+function test(){
+    console.log('cant touch this');
+    return 'this is a test function';
+}
+
+console.log(test());
+
+var hejsa = 1;
+var farvel = 2;
+
+console.log(hejsa, farvel);
