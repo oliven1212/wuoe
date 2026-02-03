fbd63aa0b9e5678e3280a3f1129f44a5bcce4a9f
Valdemar <pizza27@live.dk>
2026-02-03 13:36:07 +0100

    03-02-2026

---
+++ MFD/03-02-2026/vue-project-1/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import { ref } from "vue";
+  import Message from "@/components/Message.vue";
+
+  const isMessageVisible = ref(true);
+</script>
+
+<template>
+  <button @click="isMessageVisible = !isMessageVisible;"> Toggle Message</button>
+
+<Message v-show="isMessageVisible" />
+</template>
+
+<style scoped></style>
---
+++ MFD/03-02-2026/vue-project-1/src/components/Message.vue
@@ -0,0 +1,31 @@
+<script setup>
+    import { ref } from 'vue';
+
+    const isPasswordVisible = ref(false);
+    const password = ref('');
+</script>
+<template>
+    <div>
+        <button @click="isPasswordVisible = !isPasswordVisible;">Show password</button>
+        <p>{{ password }}</p>
+        <input 
+           :type="isPasswordVisible ? 'text': 'password'" 
+            v-model="password"
+            :class="{ 
+                error: password.length < 8,
+                success: password.length >= 8 }"
+        />
+    </div>
+</template>
+
+<style scoped>
+    input{
+        outline: none;
+    }
+    .error{
+        border: red 1px solid;
+    }
+    .success{
+        border: lightgreen 1px solid;
+    }
+</style>
