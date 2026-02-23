6ca0a6f5889eef78ccf1c165111dc3ffc72c6c5c
Valdemar <pizza27@live.dk>
2026-02-23 08:41:00 +0100

    ...

---
+++ MFD/17-02-2026/vue-project/src/views/ProductCreateView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+<div>
+    Product create View - Sparkle
+</div>
+</template>
---
+++ MFD/17-02-2026/vue-project/src/views/ProductView.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+<div>
+    <RouterLink :to="{ name: 'product-create' }">Opret nyt produkt</RouterLink>
+    Product View - Sparkle
+    <hr>
+    <RouterView/>
+</div>
+</template>
