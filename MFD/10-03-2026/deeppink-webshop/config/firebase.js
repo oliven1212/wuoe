738b589eb778b4495bd50ab403f6b3551dbdc1d0
Valdemar <pizza27@live.dk>
2026-03-10 13:08:33 +0100

    firebase setup

---
+++ MFD/10-03-2026/deeppink-webshop/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDdounh9lD1AXVwOnA8PGoV5nDq0Dvxt-g",
+  authDomain: "wuoe-mfd.firebaseapp.com",
+  projectId: "wuoe-mfd",
+  storageBucket: "wuoe-mfd.firebasestorage.app",
+  messagingSenderId: "226109053555",
+  appId: "1:226109053555:web:9a53f0d2bdf5298803e806"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
